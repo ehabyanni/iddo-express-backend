94c670c1df5d3159efd6351a7d5feaa6a79b8581
ehabyanni <ehabayanni@gmail.com>
2026-08-10 16:30:28 +0300

    ref: remove desc column from image accordion

--- src/types/imageAccordion.ts
+++ src/types/imageAccordion.ts
@@ -4,12 +4,10 @@ export interface FormattedImages {
   id: number;
   url: string;
   title: string;
-  description: string;
 }
 
 export const formatImages = (img: PrismaImg): FormattedImages => ({
   id: img.id,
   url: img.url,
   title: img.title,
-  description: img.description,
 });
