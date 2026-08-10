75a41f3d55ccd77513f393e7a1d07e17db34a53c
ehabyanni <ehabayanni@gmail.com>
2026-08-10 10:03:12 +0300

    ref: CORS accissbility

--- src/server.ts
+++ src/server.ts
@@ -16,7 +16,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'https://iddo-delta.vercel.app'],
+  credentials: true
+}));
 app.use(express.json());
 
 // ----------------------------------------------------
